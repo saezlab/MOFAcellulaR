169384c01d1ecbbf778c7cc3a665bffd08b4094e
roramirezf <roramirezf@uni-heidelberg.de>
2023-03-07 16:25:05 +0100

    added post-proc functions

---
+++ R/get_associations.R
@@ -0,0 +1,99 @@
+#' Associate factors to covariates of interest
+#'
+#' @description
+#' Performs Analysis of Variance (ANOVA) or linear models to associate
+#' factor scores with covariates of interest provided by the user
+#'
+#' @details
+#' Given a covariate of interest and a defined test, this function tests for associations with factor scores. For
+#' categorical tests, ANOVAs are fitted, while for continous variables, linear models. P-values are corrected
+#' using the Benjamini-Hochberg procedure.
+#'
+#' @param model A MOFA2 model.
+#' @param metadata A data frame containing the annotations of the samples included in the MOFA model.
+#' @param sample_id_column A string character that refers to the column in `metadata` where the sample identifier is located.
+#' @param test_variable A string character that refers to the column in `metadata` where the covariate to be tested is located.
+#' @param test_type A string character ("categorical", "continuous")
+#' @param group Boolean flag TRUE/FALSE, to specify if a grouped MOFA model is provided.
+#'
+#'
+#' @return A dataframe in a tidy format containing the p-values of the association tests per factor
+#' @export
+#'
+#' @import MOFA2
+#' @import tibble
+#' @import dplyr
+#' @import broom
+#' @import stats
+#'
+#' @examples
+#' inputs_dir <- base::system.file("data", package = "MOFAcellulaR")
+#' model <- MOFA2::load_model(file.path(inputs_dir, "testmodel.hdf5"))
+#' metadata <- readRDS(file.path(inputs_dir, "testmetadata.rds"))
+#' metadata$var <- stats::rnorm(nrow(metadata))
+#'
+#' categorical_assoc <- get_associations(model = model,
+#'                                       metadata = metadata,
+#'                                       sample_id_column = "sample",
+#'                                       test_variable = "patient_group",
+#'                                       test_type = "categorical",
+#'                                       group = FALSE)
+#'
+#' continuous_assoc <- get_associations(model = model,
+#'                                       metadata = metadata,
+#'                                       sample_id_column = "sample",
+#'                                       test_variable = "var",
+#'                                       test_type = "continuous",
+#'                                       group = FALSE)
+get_associations <- function(model,
+                             metadata,
+                             sample_id_column,
+                             test_variable,
+                             test_type = "categorical",
+                             group = FALSE) {
+
+  factors <- get_tidy_factors(model = model,
+                              metadata = metadata,
+                              factor = "all",
+                              group = group,
+                              sample_id_column = sample_id_column)
+
+  # Get factors associated with patient group
+  factors <- factors %>%
+    dplyr::select_at(c("sample", test_variable, "Factor", "value")) %>%
+    dplyr::group_by(Factor) %>%
+    tidyr::nest() %>%
+    dplyr::mutate(pvalue = purrr::map(data, function(dat) {
+
+      # Fit ANOVAs if testing variable is categorical
+      if(test_type == "categorical") {
+
+        factor_aov <- stats::aov(as.formula(paste0("value ~ ", test_variable)), data = dat) %>%
+          broom::tidy() %>%
+          dplyr::filter(term == test_variable) %>%
+          dplyr::select(term, p.value)
+
+        return(factor_aov)
+
+      } else {
+        # Fit linear model if testing variable is continous
+        factor_lm <- stats::lm(as.formula(paste0("value ~ ", test_variable)), data = dat) %>%
+          broom::tidy() %>%
+          dplyr::filter(term == test_variable) %>%
+          dplyr::select(term, p.value)
+
+        return(factor_lm)
+
+      }
+
+    })) %>%
+    tidyr::unnest(pvalue) %>%
+    dplyr::ungroup() %>%
+    dplyr::mutate(adj_pvalue = stats::p.adjust(p.value))
+
+  expl_var <- factors %>%
+    dplyr::select(Factor, term, p.value, adj_pvalue)
+
+  return(expl_var)
+
+}
---
+++ R/get_tidy_factors.R
@@ -0,0 +1,122 @@
+#' Extract factor scores from a model in a tidy format with meta data
+#'
+#' @description
+#' Generates a tidy dataframe for factors of interest useful for plotting functions.
+#'
+#' @details
+#'
+#' This function simplifies the extraction of factor scores from MOFA models.
+#'
+#' @param model A MOFA2 model.
+#' @param metadata A data frame containing the annotations of the samples included in the MOFA model.
+#' @param factor A string character to define the factor to be extracted. Alternatively, "all" to get all factors.
+#' @param group Boolean flag TRUE/FALSE, to specify if a grouped MOFA model is provided.
+#' @param sample_id_column A string character that refers to the column in `metadata` where the sample identifier is located.
+#'
+#'
+#' @return A dataframe in a tidy format containing the factor scores per sample together with metadata
+#' @export
+#'
+#' @import MOFA2
+#' @import tibble
+#' @import dplyr
+#'
+#' @examples
+#' inputs_dir <- base::system.file("data", package = "MOFAcellulaR")
+#' model <- MOFA2::load_model(file.path(inputs_dir, "testmodel.hdf5"))
+#' metadata <- readRDS(file.path(inputs_dir, "testmetadata.rds"))
+#' all_factors <- get_tidy_factors(model = model,
+#'                                 metadata = metadata,
+#'                                 factor = "all",
+#'                                 sample_id_column = "sample")
+#' Factor3 <- get_tidy_factors(model = model,
+#'                             metadata = metadata,
+#'                             factor = "Factor3",
+#'                             sample_id_column = "sample")
+get_tidy_factors <-  function(model,
+                              metadata,
+                              factor,
+                              group = FALSE,
+                              sample_id_column = "sample") {
+
+  if(group) {
+
+    if(factor == "all") {
+      #Here we return all factors and bind the distinct groups
+      factor_scores <- MOFA2::get_factors(model, factors = "all") %>%
+        base::do.call(rbind, .)
+
+    } else {
+      #Here we return specific factors
+      factor_scores <- MOFA2::get_factors(model, factors = "all") %>%
+        base::do.call(rbind, .)[, factor, drop= F]
+
+    }
+
+  } else {
+
+    if(factor == "all") {
+      #Here we return all factors and bind the distinct groups
+      factor_scores <- MOFA2::get_factors(model, factors = "all")[[1]]
+    } else {
+      #Here we return specific factors
+      factor_scores <- MOFA2::get_factors(model, factors = "all")[[1]][,factor, drop= F]
+    }
+
+  }
+
+  # Merge with provided meta_data
+  factor_scores <- factor_scores %>%
+    base::as.data.frame() %>%
+    tibble::rownames_to_column(sample_id_column) %>%
+    dplyr::left_join(metadata,
+              by = sample_id_column)  %>%
+    tidyr::pivot_longer(-colnames(metadata),
+                        names_to = "Factor") %>%
+    dplyr::rename("sample" = sample_id_column)
+
+  return(factor_scores)
+
+}
+
+#' Extract feature weights scores from a model in a tidy format for a factor of interest
+#'
+#' @description
+#' Generates a tidy dataframe of feature weights for factors of interest useful for plotting function and enrichment analysis
+#'
+#' @details
+#'
+#' This function simplifies the extraction of factor feature weights from MOFA models.
+#'
+#' @param model A MOFA2 model.
+#' @param factor A string character to define the factor to be extracted.
+#'
+#'
+#' @return A dataframe in a tidy format containing the factor feature weights
+#' @export
+#'
+#' @import MOFA2
+#' @import dplyr
+#'
+#' @examples
+#' inputs_dir <- base::system.file("data", package = "MOFAcellulaR")
+#' model <- MOFA2::load_model(file.path(inputs_dir, "testmodel.hdf5"))
+#' gene_weights <- get_geneweights(model = model,
+#'                                 factor = "Factor1")
+get_geneweights <- function(model, factor) {
+
+  factor_loadings <- MOFA2::get_weights(model, as.data.frame = T) %>%
+    base::as.data.frame() %>%
+    #Here we take the prefix out
+    dplyr::mutate(feature = strsplit(as.character(feature), "_") %>%
+                    map_chr(., ~ .x[[2]]),
+                  ctype = strsplit(as.character(view), "_") %>%
+                    map_chr(., ~ .x[[1]])) %>%
+    dplyr::rename("factors" = factor) %>%
+    dplyr::select(-view) %>%
+    dplyr::filter(factors %in% factor) %>%
+    dplyr::select(-factors)
+
+  return(factor_loadings)
+
+}
