d5ddd4e37859e778105e64426c2a1daeadb7bacb
roramirezf <roramirezf@uni-heidelberg.de>
2023-04-18 10:05:54 +0200

    [FUNC] added centering function

--- R/create_init_exp.R
+++ R/create_init_exp.R
@@ -97,3 +97,67 @@ pb_dat2MOFA <- function(pb_dat_list) {
   return(pb_red)
 
 }
+
+
+#' Center view-data
+#'
+#' @description
+#' Centers each element of a list of SummarizedExperiments
+#'
+#' @details
+#'
+#' Given that the MOFA model in general uses centered data, when interested in projecting new
+#' data to a new manifold, it is needed to perform centering.
+#'
+#' @param pb_dat_list List of SummarizedExperiment generated from `MOFAcellulaR::filt_profiles()`
+#'
+#' @return A named list of SummarizedExperiments per cell type provided with centered pseudobulk profiles
+#' @export
+#'
+#' @import SummarizedExperiment
+#' @import S4Vectors
+#' @import purrr
+#' @import scran
+#' @import tidyr
+#' @import tibble
+#' @import dplyr
+#'
+#' @examples
+#' inputs_dir <- base::system.file("extdata", package = "MOFAcellulaR")
+#' load(file.path(inputs_dir, "testpbcounts.rda"))
+#' load(file.path(inputs_dir, "testcoldata.rda"))
+#' pb_obj <- create_init_exp(counts = testpbcounts,  coldata = testcoldata)
+#'
+#' ct_list <- filt_profiles(pb_dat = pb_obj,
+#'                          cts = c("Fib","CM"),
+#'                          ncells = 5,
+#'                          counts_col = "cell_counts",
+#'                          ct_col = "cell_type")
+#'
+#' ct_list <- filt_gex_byexpr(pb_dat_list = ct_list,
+#'                            min.count = 5,
+#'                            min.prop = 0.25)
+#'
+#' ct_list <- tmm_trns(pb_dat_list = ct_list,
+#'                     scale_factor = 1000000)
+#'
+#' ct_list <- center_views(pb_dat_list = ct_list)
+#'
+#' multiview_dat <- pb_dat2MOFA(pb_dat_list = ct_list)
+center_views <- function(pb_dat_list) {
+
+  pb_red <- purrr::map(pb_dat_list, function(x) {
+
+    dat <- SummarizedExperiment::assay(x, "logcounts") %>% t() # genes in columns now
+
+    scaled_dat <- base::scale(dat, scale = FALSE) %>% t() # genes in rows now
+
+    SummarizedExperiment::assay(x, "logcounts") <- scaled_dat #replace assay
+
+    return(x)
+
+  })
+
+  return(pb_red)
+
+}
