52929f44c59ce7908537a5272c3d7f804b4e017a
Ricardo Ramirez <roramirezf@uni-heidelberg.de>
2023-06-29 13:13:51 +0200

    [ISSUE] added filtering functions

--- R/filtering_basic.R
+++ R/filtering_basic.R
@@ -126,3 +126,107 @@ filt_gex_byexpr <- function(pb_dat_list, min.count, min.prop) {
   return(pb_dat_red)
 
 }
+
+#' Filter views with not enough features
+#'
+#' @description
+#' Filter complete views based on their number of profiled features
+#'
+#' @details
+#'
+#' This function allows the user to control the number of minimum features per view
+#'
+#' @param pb_dat_list List of SummarizedExperiment generated from `MOFAcellulaR::filt_profiles()`
+#' @param ngenes Numeric, minimum number of features per view.
+#'
+#' @return A named list of SummarizedExperiments per cell type provided with filtered pseudobulk profiles
+#' @export
+#'
+#' @import SummarizedExperiment
+#' @import S4Vectors
+#' @import purrr
+#'
+#' @examples
+#' inputs_dir <- base::system.file("extdata", package = "MOFAcellulaR")
+#' load(file.path(inputs_dir, "testpbcounts.rda"))
+#' load(file.path(inputs_dir, "testcoldata.rda"))
+#'
+#' pb_obj <- create_init_exp(counts = testpbcounts,
+#'                           coldata = testcoldata)
+#'
+#' ct_list <- filt_profiles(pb_dat = pb_obj,
+#'                          cts = c("Fib","CM"),
+#'                          ncells = 5,
+#'                          counts_col = "cell_counts",
+#'                          ct_col = "cell_type")
+#'
+#' ct_list <- filt_gex_byexpr(pb_dat_list = ct_list,
+#'                            min.count = 5,
+#'                            min.prop = 0.25)
+#'
+#' ct_list <- filt_views_bygenes(pb_dat_list = ct_list,
+#'                               ngenes = 15)
+#'
+filt_views_bygenes <- function(pb_dat_list,
+                               ngenes) {
+
+  view_count <- pb_dat_list %>%
+    purrr::map_dbl(~ .x %>% nrow())
+
+  view_count <- view_count[view_count >= ngenes]
+
+  views <- names(view_count)
+
+  return(pb_dat_list[views])
+
+}
+
+#' Filter views with not enough samples
+#'
+#' @description
+#' Filter complete views based on their number of profiled samples
+#'
+#' @details
+#'
+#' This function allows the user to control the number of minimum samples per view
+#'
+#' @param pb_dat_list List of SummarizedExperiment generated from `MOFAcellulaR::filt_profiles()`
+#' @param nsamples Numeric, minimum number of samples per view.
+#'
+#' @return A named list of SummarizedExperiments per cell type provided with filtered pseudobulk profiles
+#' @export
+#'
+#' @import SummarizedExperiment
+#' @import S4Vectors
+#' @import purrr
+#'
+#' @examples
+#' inputs_dir <- base::system.file("extdata", package = "MOFAcellulaR")
+#' load(file.path(inputs_dir, "testpbcounts.rda"))
+#' load(file.path(inputs_dir, "testcoldata.rda"))
+#'
+#' pb_obj <- create_init_exp(counts = testpbcounts,
+#'                           coldata = testcoldata)
+#'
+#' ct_list <- filt_profiles(pb_dat = pb_obj,
+#'                          cts = c("Fib","CM"),
+#'                          ncells = 5,
+#'                          counts_col = "cell_counts",
+#'                          ct_col = "cell_type")
+#'
+#' ct_list <- filt_views_bysamples(pb_dat_list = ct_list,
+#'                               nsamples = 2)
+#'
+filt_views_bysamples <- function(pb_dat_list,
+                                 nsamples) {
+
+  view_count <- pb_dat_list %>%
+    purrr::map_dbl(~ .x %>% ncol())
+
+  view_count <- view_count[view_count >= nsamples]
+
+  views <- names(view_count)
+
+  return(pb_dat_list[views])
+
+}
