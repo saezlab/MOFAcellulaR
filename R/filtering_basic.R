e9e48cbde20f598cfa0cd2a747fb67e7ec938968
Ricardo Ramirez <roramirezf@uni-heidelberg.de>
2023-07-18 16:32:08 +0200

    [UTIL] added coverage filter

--- R/filtering_basic.R
+++ R/filtering_basic.R
@@ -230,3 +230,64 @@ filt_views_bysamples <- function(pb_dat_list,
   return(pb_dat_list[views])
 
 }
+
+#' Filter samples that have a limited coverage of features in a view
+#'
+#' @description
+#' Excludes samples from views with a coverage lower than the one stated
+#'
+#' @details
+#'
+#' This function allows the user to exclude samples that have a limited number of features that could create potential outliers
+#'
+#' @param pb_dat_list List of SummarizedExperiment generated from `MOFAcellulaR::filt_profiles()`
+#' @param prop_coverage Numeric, minimum proportion of coverage of features (different from 0).
+#'
+#' @return A named list of SummarizedExperiments per cell type provided with filtered pseudobulk profiles
+#' @export
+#'
+#' @import SummarizedExperiment
+#' @import S4Vectors
+#' @import purrr
+#'
+#' @examples
+#' inputs_dir <- base::system.file("extdata", package = "MOFAcellulaR")
+#' load(file.path(inputs_dir, "testpbcounts.rda"))
+#' load(file.path(inputs_dir, "testcoldata.rda"))
+#'
+#' pb_obj <- create_init_exp(counts = testpbcounts,
+#'                           coldata = testcoldata)
+#'
+#' ct_list <- filt_profiles(pb_dat = pb_obj,
+#'                          cts = c("Fib","CM"),
+#'                          ncells = 5,
+#'                          counts_col = "cell_counts",
+#'                          ct_col = "cell_type")
+#'
+#' ct_list <- filt_gex_byexpr(pb_dat_list = ct_list,
+#'                            min.count = 5,
+#'                            min.prop = 0.25)
+#'
+#' ct_list <- filt_views_bygenes(pb_dat_list = ct_list,
+#'                               ngenes = 15)
+#'
+#' ct_list <- filt_samples_bycov(pb_dat_list = ct_list,
+#'                               prop_coverage = 0.9)
+#'
+#'
+filt_samples_bycov <- function(pb_dat_list, prop_coverage = 0.9) {
+
+  pb_dat_list_new <- purrr::map(pb_dat_list, function(pb_obj) {
+
+    mat <- SummarizedExperiment::assay(pb_obj, "counts")
+
+    sample_vect <- (mat != 0) %>% colSums(.)/nrow(mat)
+
+    sel_samples <- names(sample_vect[which(sample_vect >= prop_coverage)])
+
+    return(pb_obj[,sel_samples])
+
+  })
+
+  return(pb_dat_list_new)
+}
